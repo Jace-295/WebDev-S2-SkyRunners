88058c7a042410301618e469ff29262fc964839a
Jace-295 <jacob.eaton22@bathspa.ac.uk>
2023-05-25 16:45:26 +0100

    SkyRunners / Purchase Page / Sorting the form and adding links to other pages

--- Code/index.js
+++ Code/index.js
@@ -1,15 +1,17 @@
-// var formData = document.forms[0];
+// ${formData.elemnts["AddressLine2"].value}
 
-// alert(formData);
-
-const submitButton = document.getElementById("submitButton");
-console.log(submitButton)
-
-submitButton.addEventListener("click",submit,false);
+let cars = document.getElementById("cars");
 
 function submit(event){
     event.preventDefault();
     var formData = document.forms[0];
-
-    alert(`First Name: ${formData.elements["fname"].value} Last Name: ${formData.elements["lname"].value}`);
+    alert(`Service :${cars.options[cars.selectedIndex].getAttribute('name')}
+Price: ${formData.elements["Options"].value}
+Address: ${formData.elements["AddressLine1"].value} ${formData.elements["AddressLine2"].value} ${formData.elements["AddressLine3"].value}
+Postcode: ${formData.elements["PostCode"].value}`)
 }
+
+
+const submission = document.getElementById("submission");
+console.log(submission)
+submission.addEventListener("click",submit,false);
