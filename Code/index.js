237c38a76b25d92cc1e58f619c457b14809f6ea4
Jace-295 <jacob.eaton22@bathspa.ac.uk>
2023-05-26 17:44:39 +0100

    SkyRunners / Purchase Page / complete layout and JS

--- Code/index.js
+++ Code/index.js
@@ -1,14 +1,26 @@
-// ${formData.elemnts["AddressLine2"].value}
-
 let cars = document.getElementById("cars");
 
 function submit(event){
     event.preventDefault();
     var formData = document.forms[0];
-    alert(`Service :${cars.options[cars.selectedIndex].getAttribute('name')}
+    alert(`Service: ${cars.options[cars.selectedIndex].getAttribute('name')}
 Price: ${formData.elements["Options"].value}
 Address: ${formData.elements["AddressLine1"].value} ${formData.elements["AddressLine2"].value} ${formData.elements["AddressLine3"].value}
-Postcode: ${formData.elements["PostCode"].value}`)
+Postcode: ${formData.elements["PostCode"].value}
+Sender: ${formData.elements["SenderName"].value}
+Message: ${formData.elements["CustomMessage"].value}
+Item: ${formData.elements["Item"].value}
+Receiver: ${formData.elements["ReceiverName"].value}
+Request: ${formData.elements["AdditionalRequests"].value}`);
+    let price=formData.elements["Options"].value;
+    let address=formData.elements["AddressLine1"].value+" "+formData.elements["AddressLine2"].value+" "+formData.elements["AddressLine3"].value;
+    let postcode=formData.elements["PostCode"].value;
+    let sender=formData.elements["SenderName"].value;
+    let message=formData.elements["CustomMessage"].value;
+    let item=formData.elements["Item"].value;
+    let receiver=formData.elements["ReceiverName"].value;
+    let request=formData.elements["AdditionalRequests"].value;
+    location.reload();
 }
 
 
